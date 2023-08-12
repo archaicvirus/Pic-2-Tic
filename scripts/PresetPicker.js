cb08471099deafd7e8759de5c2af14013e639560
ArchaicVirus <richardhall1029@gmail.com>
2023-08-11 23:19:23 -0400

    Add files via upload

--- scripts/PresetPicker.js
+++ scripts/PresetPicker.js
@@ -15,51 +15,44 @@ class PresetPicker {
   }
 
   createDropdownElement() {
-    let dropdown = document.createElement('div');
-    dropdown.classList.add('dropdown');
+    this.dropdown = document.createElement('div');
+    this.dropdown.id = 'dropdown';
+    this.dropdown.classList.add('dropdown');
+    this.dropdown.innerText = 'Select Palette';
+    this.dropdownHeader = document.createElement('input');
+    this.dropdownHeader.classList.add('dropdown-header');
+    this.dropdownHeader.value = 'Select Palette';
+    this.dropdownHeader.addEventListener('click', (e) => {
+      e.preventDefault;
+      this.dropdownHeader.value = '';
+      this.handleDropdownToggle();
+    });
 
-    let dropdownHeader = document.createElement('div');
-    dropdownHeader.classList.add('dropdown-header');
-    dropdownHeader.innerText = 'Select Palette';
-    dropdownHeader.addEventListener('click', () => this.handleDropdownToggle());
-    let dropdownIcon = document.createElement('span');
-    dropdownIcon.classList.add('dropdown-icon');
-    dropdownIcon.classList.add('open');
-    dropdownHeader.appendChild(dropdownIcon);
-    let dropdownList = document.createElement('ul');
-    dropdownList.classList.add('dropdown-list');
-    dropdownList.style.display = 'none';
+    this.dropdownHeader.addEventListener('focusout', (e) => { 
+      this.dropdownHeader.value = 'Select Palette';
+      console.log('clicked off search');
+      this.isDropdownOpen = false;
+      this.updateDropdownList(Presets);
+    });
 
-    for (let [paletteName, paletteColors] of Object.entries(Presets)) {
-      let listItem = document.createElement('li');
-      listItem.classList.add('dropdown-item');
-      listItem.addEventListener('click', () => this.handlePaletteChange(paletteName, paletteColors));
-
-      let palettePreview = document.createElement('div');
-      palettePreview.classList.add('palette-preview');
-
-      let paletteNameSpan = document.createElement('div');
-      paletteNameSpan.classList.add('palette-name');
-      paletteNameSpan.innerText = paletteName;
-
-      let iconGridSmall = document.createElement('div');
-      iconGridSmall.classList.add('icon-grid-small');
-
-      for (let i = 0; i < 16; i++) {
-        let iconSmall = document.createElement('div');
-        iconSmall.classList.add('icon-small');
-        iconSmall.style.backgroundColor = `#${paletteColors.slice(i * 6, i * 6 + 6)}`;
-        iconGridSmall.appendChild(iconSmall);
+    this.dropdownHeader.addEventListener('input', (e) => {
+      console.log('typing - ' + this.dropdownHeader.value);
+      if (this.dropdownHeader.value === '') {
+        console.log('empty input');
+        this.updateDropdownList(Presets);
+      }else{
+        this.handleSearch(this.dropdownHeader.value);
       }
+    });
+    this.dropdownIcon = document.createElement('span');
+    this.dropdownIcon.classList.add('dropdown-icon');
+    this.dropdownIcon.classList.add('open');
+    this.dropdownHeader.appendChild(this.dropdownIcon);
+    this.dropdownList = this.createDropdownList(Presets);
 
-      listItem.appendChild(paletteNameSpan);
-      listItem.appendChild(iconGridSmall);
-      dropdownList.appendChild(listItem);
-    }
-
-    dropdown.appendChild(dropdownHeader);
-    dropdown.appendChild(dropdownList);
-    return dropdown;
+    this.dropdown.appendChild(this.dropdownHeader);
+    this.dropdown.appendChild(this.dropdownList);
+    return this.dropdown;
   }
 
   setupDropdown() {
@@ -73,10 +66,8 @@ class PresetPicker {
 
   handleDropdownToggle() {
     this.isDropdownOpen = !this.isDropdownOpen;
-    let dropdownList = this.dropdownElement.querySelector('.dropdown-list');
-    dropdownList.style.display = this.isDropdownOpen ? 'block' : 'none';
-    let dropdownIcon = this.dropdownElement.querySelector('.dropdown-icon');
-    dropdownIcon.classList.toggle('open', this.isDropdownOpen);
+    this.dropdownList.style.display = this.isDropdownOpen ? 'block' : 'none';
+    this.dropdownIcon.classList.toggle('open', this.isDropdownOpen);
   }
 
   handlePaletteChange(paletteName, paletteColors) {
@@ -90,13 +81,63 @@ class PresetPicker {
 
   closeDropdown() {
     this.isDropdownOpen = false;
-    let dropdownList = this.dropdownElement.querySelector('.dropdown-list');
-    dropdownList.style.display = 'none';
-    let dropdownIcon = this.dropdownElement.querySelector('.dropdown-icon');
-    dropdownIcon.classList.remove('open');
+    this.dropdownList.style.display = 'none';
+    this.dropdownIcon.classList.remove('open');
   }
 
   getPresetPickerElement() {
     return this.container;
+  }
+
+  handleSearch(searchTerm) {
+    const matchedPalettes = {};
+    searchTerm = searchTerm.toLowerCase().trim();
+    for (let [paletteName, paletteColors] of Object.entries(Presets)) {
+      if (paletteName.toLowerCase().includes(searchTerm)) {
+        matchedPalettes[paletteName] = paletteColors;
+      }
+  }
+    console.log(matchedPalettes);
+    this.updateDropdownList(matchedPalettes);
+  }
+
+  updateDropdownList(palettes) {
+    let searchList = this.createDropdownList(palettes);
+    if (this.dropdownList && this.dropdown.contains(this.dropdownList)) {
+      this.dropdown.removeChild(this.dropdownList);
+    }
+    this.dropdownList = searchList;
+    this.dropdown.appendChild(this.dropdownList);
+    if (this.isDropdownOpen) {
+      this.dropdownList.style.display = 'block';
+    }
+}
+
+  createDropdownList(palettes) {
+    let root = document.createElement('ul');
+    root.classList.add('dropdown-list');
+    root.style.display = 'none';
+    for (let [paletteName, paletteColors] of Object.entries(palettes)) {
+      let listItem = document.createElement('li');
+      listItem.classList.add('dropdown-item');
+      listItem.addEventListener('click', () => this.handlePaletteChange(paletteName, paletteColors));
+      let palettePreview = document.createElement('div');
+      palettePreview.classList.add('palette-preview');
+      let paletteNameSpan = document.createElement('div');
+      paletteNameSpan.classList.add('palette-name');
+      paletteNameSpan.innerText = paletteName;
+      let iconGridSmall = document.createElement('div');
+      iconGridSmall.classList.add('icon-grid-small');
+      for (let i = 0; i < 16; i++) {
+        let iconSmall = document.createElement('div');
+        iconSmall.classList.add('icon-small');
+        iconSmall.style.backgroundColor = `#${paletteColors.slice(i * 6, i * 6 + 6)}`;
+        iconGridSmall.appendChild(iconSmall);
+      }
+    listItem.appendChild(paletteNameSpan);
+    listItem.appendChild(iconGridSmall);
+    root.appendChild(listItem);
+    }
+    return root
   }
 }
